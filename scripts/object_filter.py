d5449c21a7ab2a7c83166060f1e85ef8d5b75b4e
Zachary Lee <zacharyianlee@gmail.com>
2019-12-18 15:24:03 -0800

    Initial commit. Class to check for points in room finished. Pointcloud handling in progress.

---
+++ scripts/object_filter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy as rp
+from room_desc.py import Room
+from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Point32
+
+class Object_Filter:
+    def __init__(self):
+        #Set up publisher and subscriber
+        rp.Subscriber("localized_pointcloud", PointCloud, self.pc_callback)
+        self.filtered_pub = rp.Publisher("filtered_poincloud", PointCloud, queue_size = 4)
+
+        #Set up room object
+        self.room = Room()
+        self.room.fill_points_array([(0,0), (0,6), (8,6), (8,0)])
+        self.room.update_poly()
+
+    #Callback function for localized_pointcloud topic. Takes in an unfiltered but localized
+    #pointcloud, copies over the
+    def pc_callback(self, data):
+        new_points = []
+        cloud_out = PointCloud()
+        cloud_out.header = data.header
+        cloud_out.channels = data.channels
+
+        #Goes through all points and only includes ones that aren't close to or
+        #outside of the walls indicated in the room object
+        for point in data.points:
+            if self.room.has_inside((point.x, point.y)):
+                new_points.append(point)
+        cloud_out.points = new_points
+
+        self.filtered_pub.publish(cloud_out)
---
+++ scripts/room_desc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from shapely.geometry import *
+
+#Class definition for a room object. Intent is to use Shapely to determine whether
+#a given point is within a certain distance of the walls of a room or outside of
+#the room. Purpose is to filter out scan data hitting the walls to make object
+#detection easier
+
+class Room:
+    def __init__(self):
+        self.points = []
+        self.edge_boxes = []
+        self.edge_margin = 0.1
+
+    #Takes in an array of XY coordinates (or potentially XYZ, which are interpreted
+    #in the same way by Shapely) and converts it to a series of XY Point objects
+    def fill_points_array(self, point_arr):
+        self.points = []
+        for new_point in point_arr:
+            self.points.append(Point(new_point[0], new_point[1]))
+
+    #Updates the room polygon with the most recent set of points
+    def update_poly(self):
+        self.room_poly = Polygon(self.points)
+        self.make_checked_box()
+
+    #Manually sets the room polygon from another polygon
+    def set_poly(self, poly_in):
+        self.room_poly = poly_in
+        self.make_checked_box()
+
+    #Creates a polygon inside the room object. We offset inwards by the number
+    #of meters specified by self.edge_margin to account for sensor noise
+    def make_checked_box(self):
+        self.checked_box = Polygon(self.room_poly.exterior.parallel_offset(self.edge_margin, 'right'))
+
+
+    #Checks if a point is past the wall or in the wall bounding box
+    def has_inside(self, point_to_check):
+        new_point = Point(point_to_check[0], point_to_check[1])
+        return self.room_poly.contains(new_point)
